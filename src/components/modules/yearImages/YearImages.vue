f233cdb5b8dc57f37136ad1d33cbecd4e695f4e4
hossi-py <yoonho.hwang@kbfg.com>
2023-10-31 21:37:41 +0900

    인피니티 스크롤 적용

--- src/components/modules/yearImages/YearImages.vue
+++ src/components/modules/yearImages/YearImages.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="year-images-container">
+  <div class="year-images-container" @scroll="handleScroll">
     <span class="title">{{ year }}년, 우리</span>
     <button @click="closePopup">X</button>
     <div
@@ -31,6 +31,11 @@ import {
   toRefs,
 } from 'vue';
 
+type ImageOptionsType = {
+  value: string;
+  images: string[];
+};
+
 export default defineComponent({
   props: {
     year: {
@@ -41,30 +46,82 @@ export default defineComponent({
 
   setup(props, { emit }) {
     const state = reactive({
-      imageOptions: [
-        {
-          value: '2014',
-          images: [
-            '2014-0.jpg',
-            '2014-0.jpg',
-            '2014-0.jpg',
-            '2014-0.jpg',
-            '2014-0.jpg',
-            '2014-0.jpg',
-            '2014-0.jpg',
-            '2014-0.jpg',
-            '2014-0.jpg',
-            '2014-0.jpg',
-            '2014-0.jpg',
-            '2014-0.jpg',
-            '2014-0.jpg',
-            '2014-0.jpg',
-            '2014-0.jpg',
-            '2014-0.jpg',
-          ],
-        },
-      ],
+      imageOptions: [] as ImageOptionsType[],
+      lastLoadedIndex: 0,
+      imagesToLoad: 0, // 로드할 이미지 개수
+      // TODO 실제로 바꿔야 함
+      maxImagesPerYear: {
+        2014: 13,
+        2015: 16,
+        2016: 16,
+        2017: 16,
+        2018: 16,
+        2019: 16,
+        2020: 16,
+        2021: 16,
+        2022: 16,
+        2023: 16,
+      } as Record<number, number>,
+      // imageOptions: [
+      //   {
+      //     value: '2014',
+      //     images: [
+      //       '2014-0.jpg',
+      //       '2014-0.jpg',
+      //       '2014-0.jpg',
+      //       '2014-0.jpg',
+      //       '2014-0.jpg',
+      //       '2014-0.jpg',
+      //       '2014-0.jpg',
+      //       '2014-0.jpg',
+      //       '2014-0.jpg',
+      //       '2014-0.jpg',
+      //       '2014-0.jpg',
+      //       '2014-0.jpg',
+      //       '2014-0.jpg',
+      //       '2014-0.jpg',
+      //       '2014-0.jpg',
+      //       '2014-0.jpg',
+      //     ],
+      //   },
+      // ],
     });
+
+    const loadMoreImages = () => {
+      const maxImages = state.maxImagesPerYear[props.year] || 0;
+
+      state.imagesToLoad = state.maxImagesPerYear[props.year];
+      // 모든 이미지 로드하면 함수 종료
+      if (state.lastLoadedIndex >= maxImages) {
+        return;
+      }
+
+      const newImages = [];
+      for (let i = 0; i < state.imagesToLoad; i++) {
+        const imageIndex = state.lastLoadedIndex + i;
+        if (imageIndex >= maxImages) {
+          break; // 이미지 최대 개수 초가화면 반복문 종료
+        }
+        // TODO jpg가 아니라 png라면?
+        newImages.push(`${props.year}-${imageIndex}.jpg`);
+      }
+
+      state.imageOptions.push({
+        value: props.year.toString(),
+        images: newImages,
+      });
+
+      // 다음 루프때는 이전꺼를 검사하면 안되기 때문
+      state.lastLoadedIndex += newImages.length;
+    };
+
+    const handleScroll = (event: Event) => {
+      const target = event.target as HTMLElement;
+
+      if (target.scrollHeight - target.scrollTop <= target.clientHeight + 500) {
+        loadMoreImages();
+      }
+    };
 
     const closePopup = () => {
       emit('close');
@@ -74,6 +131,8 @@ export default defineComponent({
     // 컴포넌트가 마운트 될 때 스크롤 잠금
     onMounted(() => {
       document.body.classList.add('no-scroll');
+      // 인피니티 스크롤 적용
+      loadMoreImages();
     });
 
     // 컴포넌트가 언마운트될 떄 스크롤 잠금 해제
@@ -81,7 +140,7 @@ export default defineComponent({
       document.body.classList.remove('no-scroll');
     });
 
-    return { ...toRefs(state), closePopup };
+    return { ...toRefs(state), handleScroll, closePopup };
   },
 });
 </script>
