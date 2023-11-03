fc0707f419cac7b061c2fed6df667373834491c8
hossi-py <yoonho.hwang@kbfg.com>
2023-11-03 23:43:46 +0900

    userAgent i플래그 추가

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,4 @@
-// userAgent 확인
 export const isiOS =
-  /iPad|iPhone|iPod/.test(navigator.userAgent) && !(window as any).MSStream;
+  /iPad|iPhone|iPod/i.test(navigator.userAgent) && !(window as any).MSStream;
 
-export const isAndroid = /Android/.test(navigator.userAgent);
+export const isAndroid = /android/i.test(navigator.userAgent);
